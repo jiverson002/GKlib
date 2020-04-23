ad4556d870faa03a090cbf104d72482ddd56e864
Jeremy Iverson <jiverson002@csbsju.edu>
2020-04-23 12:48:55 -0500

    More modernizing of CMakeLists

--- GKlibSystem.cmake
+++ GKlibSystem.cmake
@@ -1,6 +1,4 @@
 # Helper modules.
-include(CheckFunctionExists)
-include(CheckIncludeFile)
 
 # Add compiler flags.
 if(MSVC)
@@ -23,45 +21,11 @@ if(CMAKE_COMPILER_IS_GNUCC)
   endif(NOT MINGW)
 # GCC warnings.
   set(GKlib_COPTIONS "${GKlib_COPTIONS} -Werror -Wall -pedantic -Wno-unused-function -Wno-unused-variable -Wno-unknown-pragmas -Wno-unused-label")
-elseif(${CMAKE_C_COMPILER_ID} MATCHES "Sun")
-# Sun insists on -xc99.
-  set(GKlib_COPTIONS "${GKlib_COPTIONS} -xc99")
 endif(CMAKE_COMPILER_IS_GNUCC)
 
 # Intel compiler
 if(${CMAKE_C_COMPILER_ID} MATCHES "Intel")
   set(GKlib_COPTIONS "${GKlib_COPTIONS} -xHost -std=c99")
-endif()
-
-# Check for features.
-check_include_file(execinfo.h HAVE_EXECINFO_H)
-if(HAVE_EXECINFO_H)
-  set(GKlib_COPTIONS "${GKlib_COPTIONS} -DHAVE_EXECINFO_H")
-endif(HAVE_EXECINFO_H)
-
-check_function_exists(getline HAVE_GETLINE)
-if(HAVE_GETLINE)
-  set(GKlib_COPTIONS "${GKlib_COPTIONS} -DHAVE_GETLINE")
-endif(HAVE_GETLINE)
-
-# Custom check for TLS.
-if(MSVC)
-   set(GKlib_COPTIONS "${GKlib_COPTIONS} -D__thread=__declspec(thread)")
-
-  # This if checks if that value is cached or not.
-  if("${HAVE_THREADLOCALSTORAGE}" MATCHES "^${HAVE_THREADLOCALSTORAGE}$")
-    try_compile(HAVE_THREADLOCALSTORAGE
-      ${CMAKE_BINARY_DIR}
-      ${CMAKE_CURRENT_SOURCE_DIR}/conf/check_thread_storage.c)
-    if(HAVE_THREADLOCALSTORAGE)
-      message(STATUS "checking for thread-local storage - found")
-    else()
-      message(STATUS "checking for thread-local storage - not found")
-    endif()
-  endif()
-  if(NOT HAVE_THREADLOCALSTORAGE)
-    set(GKlib_COPTIONS "${GKlib_COPTIONS} -D__thread=")
-  endif()
 endif()
 
 # Finally set the official C flags.
